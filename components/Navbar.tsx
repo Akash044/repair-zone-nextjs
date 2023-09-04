185fe9bf11b892dcf77fafcc86b46a63a6418b63
Akash374 <akash.hossain@dataphilics.in>
2023-09-04 08:13:41 +0600

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  const classNav =
+    "px-2 py-2 rounded-3xl hover:cursor-pointer transition duration-500 hover:bg-violet-500 hover:text-white";
+  return (
+    <main className="container mx-auto px-2 py-1 my-2 bg-violet-200 border-violet-200 border-2 rounded-3xl">
+      <nav className="flex justify-between items-center font-bold">
+        <div className={`${classNav} items-center `}>LOGO</div>
+        <ul className="flex justify-evenly  gap-2 font-bold">
+          <li className={classNav}>Home</li>
+          <li className={classNav}>Dashboard</li>
+          <li className={classNav}>Contact</li>
+          <li className={classNav}>Login</li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
+
+export default Navbar;
